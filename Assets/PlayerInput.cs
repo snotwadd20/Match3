07771ea0a7613be71992c4bbba0cd34933363d18
Mike Stout <stout@uselessopinions.com>
2016-05-05 13:42:40 -0700

    Tiles animate before swapping.

--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -11,6 +11,65 @@ namespace Useless.Match3
         private Vector3 activeTileOriginalScale;
 
         public Match3 gameManager;
+        
+        void AttemptMove(RaycastHit2D hit)
+        {
+            if (hit && hit.collider.gameObject != activeTile)
+            {
+                if (Vector2.Distance(activeTile.transform.position, hit.collider.gameObject.transform.position) <= 1.25f)
+                {
+                    //Scale it back down before we move it
+                    activeTile.transform.localScale = activeTileOriginalScale;
+
+                    Tile tile1 = activeTile.GetComponent<Tile.TileReference>().owner;
+                    Tile tile2 = hit.collider.gameObject.GetComponent<Tile.TileReference>().owner;
+
+                    StartCoroutine(Move(tile1, tile2));
+                }//if
+            }//if 
+            activeTile.transform.localScale = activeTileOriginalScale;
+            activeTile = null;
+        }//AttemptMove
+
+        IEnumerator Move(Tile tile1, Tile tile2)
+        {
+            UPoint tile1Pos = tile1.position;
+            UPoint tile2Pos = tile2.position;
+
+            //Do movement tweening on the tile arts
+            //System.Action doSwaponCallback = () => { gameManager.GridSwap(tile1Pos, tile2Pos); };
+
+            //Hashtable tile1Hash = iTween.Hash("position", tile2Pos, "time", 0.5f);
+            //iTween.MoveTo(tile1.art, tile1Hash);
+            tile1.art.MoveTo(tile2Pos, 0.5f, 0);
+            //Hashtable tile2Hash = iTween.Hash("position", tile1Pos, "time", 0.5f);
+            //iTween.MoveTo(tile2.art, tile2Hash);
+            tile2.art.MoveTo(tile1Pos, 0.5f, 0);
+
+            yield return new WaitForSeconds(0.5f);
+
+            //Move them back into place for now
+            tile1.position = tile1Pos;
+            tile2.position = tile2Pos;
+            tile1.art.SetActive(false);
+            tile2.art.SetActive(false);
+
+            //Then swap the tiles, which will create new arts for each of the appropriate type
+            gameManager.GridSwap(tile1Pos, tile2Pos);
+            yield return new WaitForEndOfFrame();
+        }//move
+
+        void SelectTile(RaycastHit2D hit)
+        {
+            if (hit)
+            {
+                activeTile = hit.collider.gameObject;
+
+                //highlight the tile here
+                activeTileOriginalScale = activeTile.transform.localScale;
+                activeTile.transform.localScale = activeTileOriginalScale * 1.2f;
+            }//if
+        }//SelectTile
 
         void Update()
         {
@@ -27,43 +86,6 @@ namespace Useless.Match3
             else if (Input.GetKeyDown(KeyCode.Mouse1))
                 activeTile = null;
         }//Update
-
-        void SelectTile(RaycastHit2D hit)
-        {
-            if (hit)
-            {
-                activeTile = hit.collider.gameObject;
-
-                //highlight the tile here
-                activeTileOriginalScale = activeTile.transform.localScale;
-                activeTile.transform.localScale = activeTileOriginalScale * 1.2f;
-            }//if
-        }//SelectTile
-
-        void AttemptMove(RaycastHit2D hit)
-        {
-            if (hit && hit.collider.gameObject != activeTile)
-            {
-                if (Vector2.Distance(activeTile.transform.position, hit.collider.gameObject.transform.position) <= 1.25f)
-                {
-                    Tile tile1 = activeTile.GetComponent<Tile.TileReference>().owner;
-                    Tile tile2 = hit.collider.gameObject.GetComponent<Tile.TileReference>().owner;
-
-                    UPoint tile1Pos = tile1.position;
-                    UPoint tile2Pos = tile2.position;
-
-                    //Do movement tweening on the tile arts
-
-                    //Then swap the tiles, which will create new arts for each of the appropriate type
-                    gameManager.GridSwap(tile1Pos, tile2Pos);
-
-                    //MOVE TILE
-                    print("TODO: MOVE TILE " + tile1Pos + " -> " + tile2Pos);
-                }//if
-            }//if 
-            activeTile.transform.localScale = activeTileOriginalScale;
-            activeTile = null;
-        }//AttemptMove  
 
     }//PlayerInput
 }//namespace
